876c4f34a5067e1b2b60899ecd1b7a56c19d81df
FranciscoJavierRicoPerez <fjrp8@alu.ua.es>
2022-02-04 11:56:53 +0100

    Inicio del proyecto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TFG-Shared-Life-2021-2022
+## Información del backend
+## Información del frontend
---
+++ sharedlife-backend/src/test/java/net/tfg/sharedlife/SharedlifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tfg.sharedlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedlifeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
