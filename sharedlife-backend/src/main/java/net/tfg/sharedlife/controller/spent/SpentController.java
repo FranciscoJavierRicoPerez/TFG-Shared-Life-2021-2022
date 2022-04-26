05816309c15556f3dedd049c135f1718c79abaa4
FranciscoJavierRicoPerez <fjrp8@alu.ua.es>
2022-04-26 16:10:50 +0200

    Creacion del listado de deudas que debo al creador de un gasto

--- sharedlife-backend/src/main/java/net/tfg/sharedlife/controller/spent/SpentController.java
+++ sharedlife-backend/src/main/java/net/tfg/sharedlife/controller/spent/SpentController.java
@@ -1,15 +1,26 @@
 package net.tfg.sharedlife.controller.spent;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import net.tfg.sharedlife.dto.DebtDTO;
 import net.tfg.sharedlife.dto.SpentDTO;
 
 public interface SpentController {
 
 	@PostMapping("/create")
 	ResponseEntity<?> createSpent(@RequestBody SpentDTO spent, @RequestParam("admin") boolean admin);
+	
+	@GetMapping("/debts")
+	ResponseEntity<List<DebtDTO>> getDebtsByUsername(@RequestParam("username") String username);
+	
+	@GetMapping("/id/{id}")
+	ResponseEntity<SpentDTO> getSpentById(@PathVariable("id") Long id);
 	
 }
--- sharedlife-backend/src/main/java/net/tfg/sharedlife/controller/spent/SpentControllerImpl.java
+++ sharedlife-backend/src/main/java/net/tfg/sharedlife/controller/spent/SpentControllerImpl.java
@@ -1,14 +1,22 @@
 package net.tfg.sharedlife.controller.spent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.tfg.sharedlife.dto.DebtDTO;
 import net.tfg.sharedlife.dto.SpentDTO;
 import net.tfg.sharedlife.service.spent.SpentService;
 
@@ -22,11 +30,30 @@ public class SpentControllerImpl implements SpentController {
 	@Autowired
 	private SpentService spentService;
 	
+	@PostMapping("/create")
 	@Override
 	public ResponseEntity<?> createSpent(SpentDTO spent, boolean admin) {
 		logger.info("Creating a new spent...");
 		spentService.createTask(spent, admin);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@GetMapping("/debts")
+	@Override
+	public ResponseEntity<List<DebtDTO>> getDebtsByUsername(@RequestParam("username") String username) {
+		logger.info("Getting all the debts of the user with username: {}", username);
+		List<DebtDTO> debts = new ArrayList<>();
+		debts = spentService.getDebtsByUsername(username);
+		return new ResponseEntity<>(debts, HttpStatus.OK);
+	}
+
+	@GetMapping("/id/{id}")
+	@Override
+	public ResponseEntity<SpentDTO> getSpentById(@PathVariable("id") Long id) {
+		logger.info("Getting the spent with id: {}", id);
+		SpentDTO spent = new SpentDTO();
+		spent = spentService.getSpentById(id);
+		return new ResponseEntity<>(spent, HttpStatus.OK);
 	}
 
 	
